8e66258ea3312e2845e53ce4a795b17847df444e
Lilesh444 <spiku923@gmail.com>
2023-11-15 19:17:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import "./index.css"
+import TodoInput from './components/Todoinput.js'
+import Todolist from './components/TodoList.js';
+function App() {
+  const [listTodo,setListTodo]=useState([]);
+  const addList = (inputText)=>{
+    if(inputText!=='')
+      setListTodo([...listTodo,inputText]);
+  }
+  const deleteListItem = (key)=>{
+    let newListTodo = [...listTodo];
+    newListTodo.splice(key,1)
+    setListTodo([...newListTodo])
+  }
+  return (
+    <div className="main-container">
+      <div className="center-container">
+        <TodoInput addList={addList}/>
+        <h1 className="app-heading">TODO</h1>
+        <hr/>
+        {listTodo.map((listItem,i)=>{
+          return (
+            <Todolist key={i} index={i} item={listItem} deleteItem={deleteListItem}/>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
